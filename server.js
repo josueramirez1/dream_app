4ba31e5dfb2e9c2743fc404ab9efbc1cbdb667de
Josue Ramirez <josue67ramirez@gmail.com>
2025-06-12 11:52:40 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import * as dotevn from "dotenv";
+import express from "express";
+import cors from "cors";
+import OpenAi from "openai";
+
+dotevn.config();
+const openai = new OpenAi({
+  apiKey: process.env.OPENAI,
+});
+const app = express();
+app.use(cors());
+app.use(express.json());
+// express does not parse json by default
+// cors is middleware that will parse json before every endpoint callback
+
+app.post("/dream", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-3",
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    const image = aiResponse.data[0].url;
+    res.status(200).send({ image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || `Something went wrong.`);
+  }
+});
+
+app.listen(8080, () => console.log("make art on http://localhost:8080/dream"));
